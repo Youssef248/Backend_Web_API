d0f9f3fd93f0eca9552420a0c078c2456324349c
Youssef248 <80095259+Youssef248@users.noreply.github.com>
2023-01-07 16:03:10 +0100

    update product

--- routes/product.js
+++ routes/product.js
@@ -13,6 +13,47 @@ router.post('/create',(req,res,next)=>{
             return res.status(500).json(err);
         }
     });
+});
+
+router.get('/read',(req,res,next)=>{
+    var query = "select * from product";
+    connection.query(query,(err,results)=>{
+        if(!err){
+            return res.status(200).json(results);
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    });
+});
+
+router.patch('/update/:id',(req,res,next)=>{
+    const id = req.params.id;
+    let product = req.body;
+    var query = "update product set name=?,description=?,price=? where id=?";
+    connection.query(query,[product.name,product.description,product.price,id],(err,results)=>{
+        if(!err){
+            if(results.affectedRows == 0){
+                return res.status(404).json({message:"Product id does not exist"});
+            }
+            return res.status(200).json({message:"Product updated successfully"});
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    });
+});
+
+router.delete('/delete/;id',(req,res,next)=>{
+    const id = req.params.id;
+    var query="delete from product where id=?";
+    connection.query(query,[id],(err,results)=>{
+        if(!err){
+            if(results.affectedRows == 0){
+                return res.status(404).json({message:"Product id does not exist"});
+            }
+        }
+    })
 })
 
 module.exports = router;
